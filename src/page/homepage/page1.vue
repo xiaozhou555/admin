bc32a2a1bc551e68065328902d319fa76453e4a8
lqz <2624136104@qq.com>
2018-04-23 09:23:36 +0800

    加入vue-schart

--- src/page/homepage/page1.vue
+++ src/page/homepage/page1.vue
@@ -1,13 +1,42 @@
 <template>
-<div id="page1">
- 111111111
-</div>
+  <div id="app">
+    <schart :canvasId="canvasId"
+            :type="type"
+            :width="width"
+            :height="height"
+            :data="data"
+            :options="options"
+    ></schart>
+  </div>
 </template>
-
 <script>
+  import Schart from 'vue-schart';
+  export default {
+    data() {
+      return {
+        canvasId: 'myCanvas',
+        type: 'line',
+        width: 500,
+        height: 400,
+        data: [
+          {name: '2014', value: 1342},
+          {name: '2015', value: 2123},
+          {name: '2016', value: 1654},
+          {name: '2017', value: 8795},
 
+        ],
+        options: {
+          autoWidth: true,                // 设置宽高自适应
+          showValue: false,               // 是否在图表中显示数值
+          bgColor: '#ffffff',             // 默认背景颜色
+          fillColor: '#1E9FFF',           // 默认填充颜色
+
+          title: 'Total sales of stores in recent years'
+        }
+      }
+    },
+    components: {
+      Schart
+    }
+  }
 </script>
-
-<style>
-
-</style>
